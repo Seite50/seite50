cd72923b1ad0eeea3a60e74430b5ed0536a05acb
Markus Hartmann <markush1986@gmail.com>
2018-05-10 12:25:23 +0200

    Basis Test-Klasse erstellt

--- api/views/test_books.py
+++ api/views/test_books.py
@@ -4,8 +4,7 @@ from rest_framework import status
 from django.urls import reverse
 from api.models.book import Book
 
-
-class CreateBookTest(APITestCase):
+class BookTestBase(APITestCase):
     """
     Test-Cases zum erstellen von Büchern
     """
@@ -42,6 +41,9 @@ class CreateBookTest(APITestCase):
             published_date=cls.published_date,
             seite50_sentence=cls.seite50_sentence)
 
+
+class CreateBookTest(BookTestBase):
+
     def test_api_can_create_a_book(self):
         """
         Es wird sichergestellt, dass ein Buch erstellt werden kann
@@ -51,42 +53,10 @@ class CreateBookTest(APITestCase):
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
 
 
-class GetBookTest(APITestCase):
+class GetBookTest(BookTestBase):
     """
     Test Cases zum lesen von Büchern
     """
-
-    def setUp(self):
-        """
-        Daten die für jeden Test auf´s neue benötigt werden 
-        """
-        self.book.save()
-
-    @classmethod
-    def setUpTestData(cls):
-        """
-        Daten die für alle Tests benötigt werden
-        """
-        cls.bookid = "1234"
-        cls.title = "TestBuch"
-        cls.description = "Ein Buch ueber das Testen"
-        cls.published_date = "2018-05-07"
-        cls.seite50_sentence = "Uch wie ist das schoen"
-
-        cls.book_data = {
-            'bookid': cls.bookid,
-            'title': cls.title,
-            'description': cls.description,
-            'published_date': cls.published_date,
-            'seite50_sentence': cls.seite50_sentence,
-        }
-
-        cls.book = Book(
-            bookid=cls.bookid,
-            title=cls.title,
-            description=cls.description,
-            published_date=cls.published_date,
-            seite50_sentence=cls.seite50_sentence)
 
     def test_api_can_get_a_book(self):
         """
@@ -97,42 +67,10 @@ class GetBookTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
-class UpdateBookTest(APITestCase):
+class UpdateBookTest(BookTestBase):
     """
     Test Cases zum lesen von Büchern
     """
-
-    def setUp(self):
-        """
-        Daten die für jeden Test auf´s neue benötigt werden 
-        """
-        self.book.save()
-
-    @classmethod
-    def setUpTestData(cls):
-        """
-        Daten die für alle Tests benötigt werden
-        """
-        cls.bookid = "1234"
-        cls.title = "TestBuch"
-        cls.description = "Ein Buch ueber das Testen"
-        cls.published_date = "2018-05-07"
-        cls.seite50_sentence = "Uch wie ist das schoen"
-
-        cls.book_data = {
-            'bookid': cls.bookid,
-            'title': cls.title,
-            'description': cls.description,
-            'published_date': cls.published_date,
-            'seite50_sentence': cls.seite50_sentence,
-        }
-
-        cls.book = Book(
-            bookid=cls.bookid,
-            title=cls.title,
-            description=cls.description,
-            published_date=cls.published_date,
-            seite50_sentence=cls.seite50_sentence)
 
     def test_api_can_update_book(self):
         """
@@ -145,42 +83,7 @@ class UpdateBookTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
-class DeleteBookTest(APITestCase):
-    """
-    Test Cases zum lesen von Büchern
-    """
-
-    def setUp(self):
-        """
-        Daten die für jeden Test auf´s neue benötigt werden 
-        """
-        self.book.save()
-
-    @classmethod
-    def setUpTestData(cls):
-        """
-        Daten die für alle Tests benötigt werden
-        """
-        cls.bookid = "1234"
-        cls.title = "TestBuch"
-        cls.description = "Ein Buch ueber das Testen"
-        cls.published_date = "2018-05-07"
-        cls.seite50_sentence = "Uch wie ist das schoen"
-
-        cls.book_data = {
-            'bookid': cls.bookid,
-            'title': cls.title,
-            'description': cls.description,
-            'published_date': cls.published_date,
-            'seite50_sentence': cls.seite50_sentence,
-        }
-
-        cls.book = Book(
-            bookid=cls.bookid,
-            title=cls.title,
-            description=cls.description,
-            published_date=cls.published_date,
-            seite50_sentence=cls.seite50_sentence)
+class DeleteBookTest(BookTestBase):
 
     def test_api_can_delete_book(self):
         """
