71936079a207d8532374cc1d7e33ab1fcb9ec1b0
Felix Schumacher <felix.schumacher@internetallee.de>
2018-05-12 12:25:22 +0200

    publications ist derzeit kein Feld in author.

--- api/serializers/author.py
+++ api/serializers/author.py
@@ -5,5 +5,5 @@ from api.models.author import Author
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ('id', 'name', 'publications')
+        fields = ('id', 'name')
         read_only_fields = ('id', 'date_created', 'date_modified')
